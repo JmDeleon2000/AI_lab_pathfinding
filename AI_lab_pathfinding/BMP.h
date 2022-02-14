8864fb454f4a07c6f680314a56cc12fbc1aafd4b
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2022-02-14 08:53:26 -0600

    interface redefinition

--- AI_lab_pathfinding/BMP.h
+++ AI_lab_pathfinding/BMP.h
@@ -17,17 +17,25 @@ struct Discrete_image
 	char** data = nullptr;
 };
 
+
 // MACROS that define the accepted values a discrete bmp data value may take
 // The value is green, representing a goal. 71 for G in ASCII.
 #define DISCRETE_BMP_GOAL	71
 // The value is black, representing a wall. 87 for W in ASCII.
 #define DISCRETE_BMP_WALL	87
 // The value is white, representing a walkable path. 80 for P in ASCII.
-#define DISCRETE_BMP_PATH	80
+#define DISCRETE_BMP_PATH	0b0000
 // The value is red, representing the start of the path. 83 for S in ASCII.
 #define DISCRETE_BMP_START	83
 // The value is purple, representing a step of the suggested path. 65 for A in ASCII.
 #define DISCRETE_BMP_AWNSER	65
+
+#define VISITED_RIGHT	0b0001
+#define VISITED_LEFT	0b0010
+#define VISITED_DOWN	0b0100
+#define VISITED_UP		0b1000
+
+
 
 
 //class that opens and holds bmp image information
--- AI_lab_pathfinding/main.cpp
+++ AI_lab_pathfinding/main.cpp
@@ -2,13 +2,9 @@
 #include "BMP.h"
 
 
-
-
-
 int main() 
 {
 	using namespace std;
-
 
 	BMP_image* x = new BMP_image("testb.bmp");
 	Discrete_image y = BMP_image::discretize(x);
@@ -16,10 +12,7 @@ int main()
 	BMP_image::output_BMP(x, "test_discretizationb.bmp");
 	delete x;
 
-	////0 00000000 00100010111100000000000
-	//int y = 0b00000000000100010111100000000000;
-	//float z;
-	//z = *(float*)&y;
-	//cout << z << endl;
+	
 	return 0;
 }
+
